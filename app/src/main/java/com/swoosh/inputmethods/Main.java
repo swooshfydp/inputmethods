91fe7e8ec19948c49dce9fd1cca696030e48f66e
Stuart Sullivan <sullivanstuart94@gmail.com>
2017-01-10 19:27:33 -0500

    Remove the overlay service from the app because it is not needed

--- app/src/main/java/com/swoosh/inputmethods/Main.java
+++ app/src/main/java/com/swoosh/inputmethods/Main.java
@@ -10,10 +10,5 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SettingsFragment settingsFragment = new SettingsFragment();
-        getFragmentManager().beginTransaction()
-                .replace(R.id.content_frame, settingsFragment)
-                .commit();
     }
 }
